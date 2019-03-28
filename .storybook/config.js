52aae40ad76df48097eaeb323b0166995fe627bf
Curtis M. Humphrey, Ph.D <curtis.humphrey@rate.com>
2019-03-28 11:58:07 -0500

    chore: updated storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,27 +1,9 @@
 import { configure } from '@storybook/react'
-import { setOptions } from '@storybook/addon-options'
-import { describe, it, beforeEach, afterEach } from 'storybook-addon-specifications'
 
-import '../framework/testing/chai_setup'
-
-window.describe = describe
-window.it = it
-window.beforeEach = beforeEach
-window.afterEach = afterEach
-
-window.__STORE__ = {
-  asyncReducers: {},
-  replaceReducer: () => {},
-  dispatch: () => {},
-}
-
-setOptions({
-  name: 'Memory Storybook',
-})
-
+// automatically import all files ending in *.stories.js
 function loadStories () {
-  const req = require.context('../src/components', true, /.stories.js$/)
-  req.keys().forEach((filename) => req(filename))
+  const req = require.context('../src/components', true, /\.stories\.js$/)
+  req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,30 +1,21 @@
-const debug = require('debug')('storybook:webpack')
-const {
-  DefinePlugin,
-} = require('@storybook/react/node_modules/webpack')
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
 const webpack_config = require('../framework/webpack/main.config')
-const globals = require('../framework/webpack/helpers/globals')
 
 const {rules} = webpack_config.module
 
 module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
   module: {
-    rules: rules.concat(
-      {
-        test: /\.js$/,
-        include: /storybook-addon-specifications/,
-        loader: 'babel-loader',
-        options: rules.find(({loader}) => loader === 'babel-loader').options,
-      },
-    ),
+    rules,
   },
   resolve: webpack_config.resolve,
-  externals: {
-    'react/addons': true,
-    'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': 'window',
-  },
-  plugins: [
-    new DefinePlugin(globals),
-  ],
 }
