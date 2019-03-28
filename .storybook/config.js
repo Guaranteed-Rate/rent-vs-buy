d1d84d1fcc2d36d82a8b32c1ca1c521b9c97556f
Curtis M. Humphrey, Ph.D <curtis.humphrey@rate.com>
2019-03-28 14:59:09 -0500

    feat: added GR font

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure } from '@storybook/react'
 
+import '../src/styles/core.scss'
+
 // automatically import all files ending in *.stories.js
 function loadStories () {
   const req = require.context('../src/components', true, /\.stories\.js$/)
